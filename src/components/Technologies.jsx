c0106dcd9a991ed5d35f06948f5407d2499bd638
akshatmikki <71835769+akshatmikki@users.noreply.github.com>
2025-02-10 14:11:08 +0530

    Update Technologies.jsx

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,10 +1,5 @@
-import { RiReactjsLine } from "react-icons/ri";
-import { FaAngular } from "react-icons/fa";
-import { FaNodeJs } from "react-icons/fa";
-import { FaSass } from "react-icons/fa";
-import { SiTailwindcss } from "react-icons/si";
-import { DiMysql } from "react-icons/di";
-import { SiMongodb } from "react-icons/si";
+import { FaHtml5, FaCss3Alt, FaJs, FaPhp, FaAws, FaWordpress } from "react-icons/fa";
+import { SiCplusplus, SiMysql, SiPython } from "react-icons/si";
 import { motion } from "framer-motion";
 
 const iconVariants = (duration) => ({
@@ -41,61 +36,32 @@ const Technologies = ({ isDarkMode }) => {
         viewport={{ once: true }}
         className="flex flex-wrap items-center justify-center gap-4"
       >
-        <motion.div
-          variants={iconVariants(2.5)}
-          initial="initial"
-          animate="animate"
-          className="rounded-2xl border-4 border-neutral-800 p-4"
-        >
-          <RiReactjsLine className="text-7xl text-cyan-400" />
+        <motion.div variants={iconVariants(2.5)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaHtml5 className="text-7xl text-orange-500" />
         </motion.div>
-        <motion.div
-          variants={iconVariants(3)}
-          initial="initial"
-          animate="animate"
-          className="rounded-2xl border-4 border-neutral-800 p-4"
-        >
-          <FaAngular className="text-7xl text-red-700" />
+        <motion.div variants={iconVariants(3)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaCss3Alt className="text-7xl text-blue-500" />
         </motion.div>
-        <motion.div
-          variants={iconVariants(5)}
-          initial="initial"
-          animate="animate"
-          className="rounded-2xl border-4 border-neutral-800 p-4"
-        >
-          <FaNodeJs className="text-7xl text-green-500" />
+        <motion.div variants={iconVariants(5)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaJs className="text-7xl text-yellow-500" />
         </motion.div>
-        <motion.div
-          variants={iconVariants(2)}
-          initial="initial"
-          animate="animate"
-          className="rounded-2xl border-4 border-neutral-800 p-4"
-        >
-          <FaSass className="text-7xl text-pink-400" />
+        <motion.div variants={iconVariants(2)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiCplusplus className="text-7xl text-blue-700" />
         </motion.div>
-        <motion.div
-          variants={iconVariants(4)}
-          initial="initial"
-          animate="animate"
-          className="rounded-2xl border-4 border-neutral-800 p-4"
-        >
-          <SiTailwindcss className="text-7xl text-cyan-400" />
+        <motion.div variants={iconVariants(4)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiMysql className="text-7xl text-cyan-900" />
         </motion.div>
-        <motion.div
-          variants={iconVariants(6)}
-          initial="initial"
-          animate="animate"
-          className="rounded-2xl border-4 border-neutral-800 p-4"
-        >
-          <DiMysql className="text-7xl text-cyan-900" />
+        <motion.div variants={iconVariants(6)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaPhp className="text-7xl text-indigo-500" />
         </motion.div>
-        <motion.div
-          variants={iconVariants(4)}
-          initial="initial"
-          animate="animate"
-          className="rounded-2xl border-4 border-neutral-800 p-4"
-        >
-          <SiMongodb className="text-7xl text-green-500" />
+        <motion.div variants={iconVariants(4)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiPython className="text-7xl text-yellow-400" />
+        </motion.div>
+        <motion.div variants={iconVariants(3)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaAws className="text-7xl text-orange-600" />
+        </motion.div>
+        <motion.div variants={iconVariants(5)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaWordpress className="text-7xl text-blue-700" />
         </motion.div>
       </motion.div>
     </div>
